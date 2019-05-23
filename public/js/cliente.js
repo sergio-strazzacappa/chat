1e513f82de03567ec63ba0ea853b01fb6410c92e
sergio-strazzacappa <sergio.strazzacappa@est.fi.uncoma.edu.ar>
2019-05-23 04:45:31 -0300

    Finalizado el login y el envío de mensajes, falta arreglar el css, el scroll y el tamaño de los contenedores

--- public/js/cliente.js
+++ public/js/cliente.js
@@ -8,6 +8,7 @@ $(document).ready(function() {
     /*******************************/
     /************ LOGIN ************/
     /*******************************/
+    console.log('Cliente iniciado');
 
     // Oculta la interfaz del chat para que
     // el usuario se registre
@@ -21,70 +22,72 @@ $(document).ready(function() {
     // Emite un evento de "nuevo_usuario" cuando un usuario
     // se registra
     $('#login').submit(function(e) {
-       //console.log('Emisión del evento nuevo_usuario');
+        console.log('Cliente envía su nombre de usuario');
 
         var usuario = $('#u').val();
 
-        //console.log('Nombre de usuario = ' + usuario);
-
-        socket.emit('nuevo_usuario', {usuario: usuario});
+        console.log('Nombre de usuario = ' + usuario);
+        console.log('Cliente emite: nuevoUsuario');
+        socket.emit('nuevoUsuario', {usuario: usuario});
         nombre = usuario;
         e.preventDefault(); // Previene la recarga de la página
 
         //return false;
+    });
 
-        // Verifica si existe el nombre de usuario utilizado
-        socket.on('nuevo_usuario', function(data) {
-            //console.log('El cliente recibe el evento nuevo_usuario');
-
-            if(data.correcto) {
-                $('#bienvenida').append('<p> Bienvenido/a ' + data.usuario + '</p>');
-                $('#login').hide();
-                $('#caja_chat').show();
-                $('#chat').show();
-            }
-            else {
-                alert ('Ya existe un usuario con el nombre ' + data.usuario);
-            }
-        });
+    // Verifica si existe el nombre de usuario utilizado
+    socket.on('enviarUsuario', function(data) {
+        console.log('El cliente: ' + nombre + ' recibe el evento enviarUsuario');
+        console.log('Validación del nombre de usuario ' + data.correcto);
+        if(data.correcto) {
+            console.log('Envío de datos correcto');
+            $('#bienvenida').append('<p> Bienvenido/a ' + data.usuario + '</p>');
+            $('#login').hide();
+            $('#caja_chat').show();
+            $('#chat').show();
+        }
+        else {
+            console.log('Envío de datos incorrecto');
+            $('#u').val('');
+            usuario = '';
+            alert ('Ya existe un usuario con el nombre ' + data.usuario);
+        }
     });
 });
 
+
 // Recibe los usuarios conectados y los agrega al html
-socket.on('usuarios', function(data) {
-    //console.log('Cliente recibe evento "usuarios"');
+socket.on('enviarListaUsuarios', function(data) {
+    console.log('Cliente recibe evento "enviarListaUsuarios"');
     var usuarios = data.usuarios;
-    //console.log("Nicks recibidos por el cliente: " + nicks);
+    console.log("Usuarios recibidos por el cliente: " + usuarios);
+    
+    $('#lista_usuarios').replaceWith(
+        '<ul id="lista_usuarios"> <p> Usuarios conectados: </p> </ul>');
+
     for(var i = 0; i < usuarios.length; i++) {
-        $('#lista_usuarios').append('<li class="usuario">' + usuarios[i] + '</li>');
+        if(usuarios[i] == nombre) {
+            $('#lista_usuarios').append('<li id="usuario_propio">' + usuarios[i] + '</li>');
+        }
+        else {
+            $('#lista_usuarios').append('<li class="usuario">' + usuarios[i] + '</li>');
+        }
     }
 });
 
-
-
-    /*socket.on('mensaje', function(data) {
-        console.log("Bienvenida a usuario");
-        console.log(data);
-        $('#mensajes').append($('<li class="mensaje">').text(data.text));
-    });
-
+$(function() {
+    // Emite un evento de mensaje y borra
+    // el mensaje que el usuario envío del campo
+    // del chat
     $('#chat').submit(function(e) {
+        console.log('Cliente: ' + nombre + ' envía evento de EnviarMensaje');
         e.preventDefault(); // Previene la recarga de la página
-
-        /* 
-         * Emite un evento de mensaje y borra
-         * el mensaje que el usuario envío del campo
-         * del chat
-         */
-       /* socket.emit('linea', $('#m').val());
+        socket.emit('enviarMensaje', {mensaje: $('#m').val(), usuario: nombre});
         $('#m').val('');
         return false;
     });
 
-    /*
-     * Captura el evento de chat message y 
-     * agrega el mensaje a la página web
-     */
-    /*socket.on('linea', function(msg, usr) {
-        $('#messages').append($('<li class="linea" style="text-decoration:none">').text(usr + ': ' + msg));
-    });*/
+    socket.on('mensaje', function(data) {
+        $('#mensajes').append($('<li class="mensaje">').text(data.usuario + ': ' + data.mensaje));
+    });
+});
--- server.js
+++ server.js
@@ -21,15 +21,17 @@ app.get('/', function(req, res) {
 // Lista de usuarios conectados
 var usuarios = new Array();
 
+console.log('Servidor iniciado');
+
 // Escucha por los eventos de conexiones
 // y el evento chat message cuando un usuario
 // ha enviado algún mensaje por el chat
 io.on('connection', function(socket) {
-	// console.log("Usuario conectado");
+	console.log("Usuario conectado");
 
-	enviar_usuarios(socket);
-	registrar_usuario(socket);
-	mensaje(socket);
+	enviarUsuarios(socket);
+	registrarUsuario(socket);
+	enviarMensaje(socket);
 	desconectar(socket);
 
 	/*socket.emit('mensaje', {text: 'Bienvenido'});
@@ -53,58 +55,61 @@ io.on('connection', function(socket) {
 
 // Envía una lista de los usuarios conectados a todos
 // los usuarios
-function enviar_usuarios(socket) {
-	socket.emit('usuarios', {usuarios: usuarios});
-	socket.broadcast.emit('usuarios', {usuarios, usuarios});
+function enviarUsuarios(socket) {
+	socket.emit('enviarListaUsuarios', {usuarios: usuarios});
+	socket.broadcast.emit('enviarListaUsuarios', {usuarios, usuarios});
 }
 
 // Registra a un nuevo usuario con la condición que el
 // nombre de usuario elegido no se haya registrado previamente
-function registrar_usuario(socket) {
-	socket.on('nuevo_usuario', function(data) {
-		//console.log('Servidor recibe evento de nuevo_usuario');
+function registrarUsuario(socket) {
+	socket.on('nuevoUsuario', function(data) {
+		console.log('Servidor recibe evento de nuevoUsuario');
 		var usuario = data.usuario;
-		//console.log("Nombre de usuario: " + usuario);
+		console.log("Nombre de usuario: " + usuario);
 		if(usuarios.indexOf(usuario) == -1) { // Usuario no registrado
 			usuarios.push(usuario);
-			// console.log("Lista de usuarios: " + usuarios);
+			console.log('Nombre de usuario no registrado');
+			console.log("Lista de usuarios: " + usuarios);
 			socket.usuario = usuario;
-			socket.emit('nuevo_usuario', {correcto: true, usuario: usuario}); // Envía el nombre de usuario al propio usuario
-			socket.broadcast.emit('nuevo_usuario', {usuario: usuario}); // Envía el nombre de usuario al resto de los usuarios
-			enviar_usuarios(socket);
+			console.log('Servidor envía al cliente: ' + usuario + ' el evento enviarUsuario');
+			socket.emit('enviarUsuario', {correcto: true, usuario: usuario}); // Envía el nombre de usuario al propio usuario
+			console.log('Servidor envía a todos los usuarios el cliente: ' + usuario);
+			socket.broadcast.emit('enviarListaUsuarios', {usuario: usuario}); // Envía el nombre de usuario al resto de los usuarios
+			enviarUsuarios(socket);
 		}
 		else {
-			socket.emit('nuevo_usuario', {correcto: false, usuario: usuario});
+			console.log('Nombre de usuario ya registrado');
+			socket.emit('enviarUsuario', {correcto: false, usuario: usuario});
 		}
+		console.log('Lista de usuarios ' + usuarios);
 	});
 }
 
-/*
- * El servidor espera la llegada de un nuevo mensaje y
- * se lo envía a todos los usuarios
- */
-function mensaje(socket) {
-	socket.on('mensaje', function(data) {
-		if(socket.nick) {
-			var mensaje = data.mensaje;
-			var nick = socket.nick;
-			socket.broadcast.emit('mensaje', {mensaje: mensaje, nick: nick});
-		}
+// El servidor espera la llegada de un nuevo mensaje y
+// se lo envía a todos los usuarios
+function enviarMensaje(socket) {
+	socket.on('enviarMensaje', function(data) {
+		console.log('Servidor recibe evento de enviarMensaje');
+		console.log('Mensaje: ' + data.mensaje);
+		var mensaje = data.mensaje;
+		var usuario = data.usuario;
+		console.log('Servidor emite evento de mensaje');
+		socket.emit('mensaje', {mensaje: mensaje, usuario: usuario})
+		socket.broadcast.emit('mensaje', {mensaje: mensaje, usuario: usuario});
 	});
 }
 
 function desconectar(socket) {
 	socket.on('disconnect', function() {
-		if(socket.nick) {
-			nicks.splice(nicks.indexOf(socket.nick), 1);
-			usuarios(socket);
-		}
+		console.log('Usuario: ' + socket.usuario + ' desconectado');
+		usuarios.splice(usuarios.indexOf(socket.usuario), 1);
+		console.log('Lista de usuarios despúes de desconexion ' + usuarios);
+		enviarUsuarios(socket);
 	});
 }
 
-/*
- * El servidor escucha en el puerto 8000
- */
+// El servidor escucha en el puerto 8000
 server.listen(8000, function() {
 	console.log('Servidor ejecutandose en http://localhost:8000');
 });
